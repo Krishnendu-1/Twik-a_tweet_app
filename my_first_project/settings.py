65d3a47331faa112474df5a7c7bdc567bd6ae9dd
Krishnendu Roy <roykrishnenduroy01@gmail.com>
2024-07-31 20:19:29 +0530

    updated

--- my_first_project/settings.py
+++ my_first_project/settings.py
@@ -76,16 +76,10 @@ WSGI_APPLICATION = 'my_first_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'/path/to/writable/directory/db.sqlite3',
+        'NAME': '.memory',
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': '/path/to/writable/directory/db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
