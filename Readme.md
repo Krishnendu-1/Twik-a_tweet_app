29b392722fa429b8fd8d97e9e36cea10453dcf27
Krishnendu Roy <roykrishnenduroy01@gmail.com>
2024-07-31 16:34:31 +0530

    UI/UX upgrade

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 # Twik-a_tweet_app
 Full stack Django project to create, edit, delete and see tweets from different usernames with Register, Login and Logout functionalities.
+
--- first_app/models.py
+++ first_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Twik(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    text=models.TextField(max_length=500)
+    text=models.TextField(max_length=150)
     photo=models.ImageField(upload_to='photos/', null=True, blank=True)# "Pillow" is used , a pythn imaging library
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
