6b4c0b2ff19f246b7c03d18d64754488a013ef73
Krishnendu Roy <roykrishnenduroy01@gmail.com>
2024-07-31 20:13:38 +0530

    updated

--- my_first_project/settings.py
+++ my_first_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'my_first_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/path/to/writable/directory/db.sqlite3',
     }
 }
 
